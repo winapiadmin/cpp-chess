535625bcf3b222587117aebe18032e8754bece34
winapiadmin <138602885+winapiadmin@users.noreply.github.com>
2025-01-07 14:51:38 +0000

    Optimized (and reduce from c++20 to c++17)

--- chess/main.cpp
+++ chess/main.cpp
@@ -1,4 +1,4 @@
-#include "../chess.h"
+#include "chess.cpp"
 #include <chrono>
 #include <iomanip>
 #include <iostream>
@@ -28,4 +28,31 @@ int main(){
     // Calculate elapsed time
     std::chrono::duration<double> duration = end - start;
     std::cout << setprecision(10)<<nodes/duration.count() <<"/"<<duration.count()<<" seconds" <<std::endl;
+using namespace chess;
+using namespace std;
+int perf(Board& board, int depth){
+    int nodes=0;
+    for (Move i:board.generate_legal_moves()){
+        board.push(i);
+        if (depth-1==0)nodes++;
+        else nodes+=perf(board,depth-1);
+        board.pop();
+    }
+    return nodes;
 }
+int main(){
+    Board board;
+
+    // Start time measurement
+    auto start = std::chrono::steady_clock::now();
+
+    int nodes=perf(board,5);
+
+    // End time measurement
+    auto end = std::chrono::steady_clock::now();
+
+    // Calculate elapsed time
+    std::chrono::duration<double> duration = end - start;
+    std::cout << setprecision(10)<<nodes/duration.count() <<"/"<<duration.count()<<" seconds" <<std::endl;
+}
+
